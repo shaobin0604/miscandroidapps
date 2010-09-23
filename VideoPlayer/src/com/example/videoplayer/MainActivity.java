c60849e96625f1de049c0cf1420b6137cda5c75d
邵彬 <shaobin0604@163.com>
2010-09-23 20:00:16 +0800

    [new]

--- VideoPlayer/src/com/example/videoplayer/MainActivity.java
+++ VideoPlayer/src/com/example/videoplayer/MainActivity.java
@@ -1,13 +1,285 @@
 package com.example.videoplayer;
 
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnBufferingUpdateListener;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnVideoSizeChangedListener;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity implements
+		OnBufferingUpdateListener, OnCompletionListener,
+		MediaPlayer.OnPreparedListener, OnVideoSizeChangedListener, SurfaceHolder.Callback {
+	private static final String TAG = MainActivity.class.getSimpleName();
+
+    private int mVideoWidth;
+    private int mVideoHeight;
+    private MediaPlayer mMediaPlayer;
+    private SurfaceView mPreview;
+    private SurfaceHolder holder;
+    private String path;
+    private Bundle extras;
+    private static final String MEDIA = "media";
+    private static final int LOCAL_AUDIO = 1;
+    private static final int STREAM_AUDIO = 2;
+    private static final int RESOURCES_AUDIO = 3;
+    private static final int LOCAL_VIDEO = 4;
+    private static final int STREAM_VIDEO = 5;
+    private boolean mIsVideoSizeKnown = false;
+    private boolean mIsVideoReadyToBePlayed = false;
+
+    private View mPlaybackLayout;
+    private ImageButton mBtnZoom;
+    
+    private int mScreenWidth;
+    private int mScreenHeight;
+    
+    private boolean mFull;
+    
+    private View.OnClickListener mClickListener = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			if (v == mBtnZoom) {
+				if (mFull) {
+					mFull = false;
+					adjustPlaybackSize();
+				} else {
+					mFull = true;
+					fullScreen();
+				}
+			}
+		}
+	};
+    /**
+     * 
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.main_player);
+        
+        getScreenSize();
+        
+        mPreview = (SurfaceView) findViewById(R.id.surface_view);
+        mPlaybackLayout = findViewById(R.id.playback_layout);
+        mBtnZoom = (ImageButton) findViewById(R.id.zoom);
+        mBtnZoom.setOnClickListener(mClickListener);
+        
+        holder = mPreview.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        extras = getIntent().getExtras();
+        
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+    
+    private void getScreenSize() {
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        mScreenWidth = dm.widthPixels;
+        mScreenHeight = dm.heightPixels;
+    }
+
+    private void playVideo(Integer Media) {
+        doCleanUp();
+        try {
+
+            switch (Media) {
+                case LOCAL_VIDEO:
+                    /*
+                     * TODO: Set the path variable to a local media file path.
+                     */
+                    path = "/sdcard/bmwad.3gp";
+                    if (path == "") {
+                        // Tell the user to provide a media file URL.
+                        Toast
+                                .makeText(
+                                        MainActivity.this,
+                                        "Please edit MediaPlayerDemo_Video Activity, "
+                                                + "and set the path variable to your media file path."
+                                                + " Your media file must be stored on sdcard.",
+                                        Toast.LENGTH_LONG).show();
+
+                    }
+                    break;
+                case STREAM_VIDEO:
+                    /*
+                     * TODO: Set path variable to progressive streamable mp4 or
+                     * 3gpp format URL. Http protocol should be used.
+                     * Mediaplayer can only play "progressive streamable
+                     * contents" which basically means: 1. the movie atom has to
+                     * precede all the media data atoms. 2. The clip has to be
+                     * reasonably interleaved.
+                     * 
+                     */
+                    path = "";
+                    if (path == "") {
+                        // Tell the user to provide a media file URL.
+                        Toast
+                                .makeText(
+                                        MainActivity.this,
+                                        "Please edit MediaPlayerDemo_Video Activity,"
+                                                + " and set the path variable to your media file URL.",
+                                        Toast.LENGTH_LONG).show();
+
+                    }
+
+                    break;
+
+
+            }
+
+            // Create a new media player and set the listeners
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setDataSource(path);
+            mMediaPlayer.setDisplay(holder);
+            mMediaPlayer.prepare();
+            mMediaPlayer.setOnBufferingUpdateListener(this);
+            mMediaPlayer.setOnCompletionListener(this);
+            mMediaPlayer.setOnPreparedListener(this);
+            mMediaPlayer.setOnVideoSizeChangedListener(this);
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+
+        } catch (Exception e) {
+            Log.e(TAG, "error: " + e.getMessage(), e);
+        }
+    }
+
+    public void onBufferingUpdate(MediaPlayer arg0, int percent) {
+        Log.d(TAG, "onBufferingUpdate percent:" + percent);
+
+    }
+
+    public void onCompletion(MediaPlayer arg0) {
+        Log.d(TAG, "onCompletion called");
+    }
+
+    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+        Log.v(TAG, "onVideoSizeChanged called");
+        if (width == 0 || height == 0) {
+            Log.e(TAG, "invalid video width(" + width + ") or height(" + height + ")");
+            return;
+        }
+        mIsVideoSizeKnown = true;
+        mVideoWidth = width;
+        mVideoHeight = height;
+        
+        adjustPlaybackSize();
+        
+        if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+            startVideoPlayback();
+        }
+    }
+    
+    private void adjustPlaybackSize() {
+    	getScreenSize();
+    	
+    	int height = mScreenHeight;
+    	int width = mScreenWidth;
+    	if ( mVideoWidth * mScreenHeight  > mScreenWidth * mVideoHeight ) {
+            //Log.i("@@@", "image too tall, correcting");
+            height = mScreenWidth * mVideoHeight / mVideoWidth;
+        } else if ( mVideoWidth * mScreenHeight  < mScreenWidth * mVideoHeight ) {
+            //Log.i("@@@", "image too wide, correcting");
+            width = height * mVideoWidth / mVideoHeight;
+        } else {
+            //Log.i("@@@", "aspect ratio is correct: " +
+                    //width+"/"+height+"="+
+                    //mVideoWidth+"/"+mVideoHeight);
+        }
+    	
+    	mPlaybackLayout.setLayoutParams(new LinearLayout.LayoutParams(width, height));
+    	mPlaybackLayout.requestLayout();
+    }
+    
+    private void fullScreen() {
+    	mPlaybackLayout.setLayoutParams(new LinearLayout.LayoutParams(
+    			LinearLayout.LayoutParams.FILL_PARENT,
+    			LinearLayout.LayoutParams.FILL_PARENT));
+    	mPlaybackLayout.requestLayout();
+    }
+    
+    
+
+    public void onPrepared(MediaPlayer mediaplayer) {
+        Log.d(TAG, "onPrepared called");
+        mIsVideoReadyToBePlayed = true;
+        if (mIsVideoReadyToBePlayed && mIsVideoSizeKnown) {
+            startVideoPlayback();
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder surfaceholder, int i, int j, int k) {
+        Log.d(TAG, "surfaceChanged called");
+
+    }
+
+    public void surfaceDestroyed(SurfaceHolder surfaceholder) {
+        Log.d(TAG, "surfaceDestroyed called");
+    }
+
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceCreated called");
+        playVideo(LOCAL_VIDEO);
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseMediaPlayer();
+        doCleanUp();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseMediaPlayer();
+        doCleanUp();
+    }
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
+    }
+
+    private void doCleanUp() {
+        mVideoWidth = 0;
+        mVideoHeight = 0;
+        mIsVideoReadyToBePlayed = false;
+        mIsVideoSizeKnown = false;
+    }
+
+    private void startVideoPlayback() {
+        Log.v(TAG, "startVideoPlayback");
+        holder.setFixedSize(mVideoWidth, mVideoHeight);
+        mMediaPlayer.start();
+    }
+    
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+    	super.onConfigurationChanged(newConfig);
+    	
+    	adjustPlaybackSize();
     }
 }
